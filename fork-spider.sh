686c1f5cce34c6b4264871df02bc09499aa83bf5
Ricky Wilson <echoquote@gmail.com>
2013-11-03 18:48:16 -0500

    Update fork-spider.sh

--- fork-spider.sh
+++ fork-spider.sh
@@ -1,7 +1,8 @@
-
+#!/bin/bash
+# Script to fork instances of meta-spider
+# pass -k as an arg to kill all instances of meta-spider
 
 function forkSpiders {
-	echo "Starting $0"
 	python ./meta-spider.py $1 >/dev/null &
 	if [ "$1" = "-k" ] 
 	then
